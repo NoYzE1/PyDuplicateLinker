8956c40f579eb6ad215225777cbf3cafdc20e86c
NoYzE1 <NoYzE@hotmail.de>
2018-01-29 21:02:03 +0100

    Update hash_files.py

--- hash_files.py
+++ hash_files.py
@@ -3,18 +3,33 @@ import os
 import sys
 BLOCKSIZE = 65536
 
-def h(r):
-    files = [os.path.join(dp, f) for dp, dn, fn in os.walk(r) for f in fn]
+def hash_files(root, fast):
+    files = [os.path.join(dp, f) for dp, dn, fn in os.walk(root) for f in fn]
     for f in files:
-        hasher = hashlib.md5()
-        with open(f, 'rb') as g:
-            b = g.read(BLOCKSIZE)
-            while len(b) > 0:
-                hasher.update(b)
-                b = g.read(BLOCKSIZE)
-        with open("hashes.txt", 'ab') as h:
-            h.write("{0} - {1}\r\n".format(f, hasher.hexdigest()).encode(encoding='utf_8', errors='replace'))
-        print("{0} - {1}".format(f, hasher.hexdigest()).encode(encoding='utf_8', errors='replace'))
-
-if len(sys.argv) > 1:
-    h(sys.argv[1].lstrip('"').rstrip('"'))
+        try:
+            hasher = hashlib.md5()
+            with open(f, 'rb') as current_file:
+                if fast:
+                    buff = current_file.read(BLOCKSIZE)
+                    hasher.update(buff)
+                else:
+                    print("Reading ", f)
+                    block = 1
+                    buff = current_file.read(BLOCKSIZE)
+                    while len(buff) > 0:
+                        print("\rReading Block...", block, end="")
+                        block += 1
+                        hasher.update(buff)
+                        buff = current_file.read(BLOCKSIZE)
+            with open("hashes.txt", 'ab') as h:
+                h.write("{0} - {1}\r\n".format(f, hasher.hexdigest()).encode(encoding='utf_8', errors='replace'))
+            print("\r", hasher.hexdigest())
+        except PermissionError:
+            continue
+        except FileNotFoundError:
+            continue
+if len(sys.argv) > 2:
+    if sys.argv[1] == "-f": # Fast Mode
+        hash_files(sys.argv[1].lstrip('"').rstrip('"'), True)
+elif len(sys.argv) > 1:
+    hash_files(sys.argv[1].lstrip('"').rstrip('"'), False)

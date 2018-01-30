ad1b9b81096f478de051387c3d90a37d00b7ce85
NoYzE1 <NoYzE@hotmail.de>
2018-01-30 20:00:22 +0100

    Create duplicate_finder.py

---
+++ duplicate_finder.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+def find_duplicates():
+    with open("hashes.txt", 'r', encoding='utf_8', errors='replace') as hashfile:
+        hashmap = []
+        for f in hashfile:
+            filename = "".join(f.rsplit(" - ", 1)[0:-1])
+            hash_value = f.rsplit(" - ", 1)[-1]
+            hashmap.append((filename, hash_value))
+    for i in range(len(hashmap)):
+        for j in range(i+1, len(hashmap)):
+            if hashmap[i][1] == hashmap[j][1]:
+                print("Duplicate found! ", hashmap[i][0], hashmap[j][0])
+find_duplicates()
